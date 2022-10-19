31af5513c791eb5e874cfd3be1eef86918dbf0db
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-10-19 11:33:44 +0300

    todo list mui icons

--- src/Pages/components/TodoList.js
+++ src/Pages/components/TodoList.js
@@ -1,12 +1,27 @@
 import React, {useState} from 'react'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+
 
 const TodoList = ({todos}) => {
+  const [clicked, setClicked] = useState(false)
 
+  const handleClick = () => {
+    setClicked(curr=> !curr)
+  }
   return (
-    <div>
-      {todos.map((item, i) => <div key={i}>{item.todo}</div>)}
+    <div className='items-container'>
+      {todos.map((item, i) =>
+      <div key={i} onClick={handleClick} className="todo-item">
+        { clicked === true ? <span><CheckCircleIcon/></span> : <span><CheckCircleOutlineIcon/></span>}
+
+        <p className={clicked ? 'item-completed' : 'item-waiting'}>{item.todo}</p>
+        <span><DeleteOutlineIcon/></span>
+        </div>
+        )}
     </div>
   )
 }
-
 export default TodoList
